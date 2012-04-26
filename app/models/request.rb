a2826a1787dd87730d8db84921f9b4c941b42e16
Seb Bacon <seb.bacon@gmail.com>
2012-04-26 09:53:19 +0100

    Annotate models with their schema (use `annotate --exclude tests,fixtures` to update)

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: requests
+#
+#  id            :integer         not null, primary key
+#  title         :string(255)
+#  status        :string(255)
+#  requestor_id  :integer
+#  created_at    :datetime        not null
+#  updated_at    :datetime        not null
+#  body          :text
+#  date_received :date            default(Tue, 24 Apr 2012), not null
+#
+
 class Request < ActiveRecord::Base
   belongs_to :requestor
   validates_presence_of :title
