ab145d88eb15fa0cb842ab60e4db2d9ec387c583
Robin Houston <robin.houston@gmail.com>
2012-04-26 11:57:41 +0100

    Ah, 20 working days is ≈28 real days, not 21.

--- db/migrate/20120426103842_change_requests_due_date_to_not_null.rb
+++ db/migrate/20120426103842_change_requests_due_date_to_not_null.rb
@@ -2,7 +2,7 @@ class ChangeRequestsDueDateToNotNull < ActiveRecord::Migration
   def up
     Request.where("due_date is null").find_each do |request|
       date_received = request.date_received || request.created_at.to_date
-      request.due_date = date_received + 21.days
+      request.due_date = date_received + 28.days
       request.save!
     end
     change_column_null(:requests, :due_date, false)
