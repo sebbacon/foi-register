61f490d5fbe09f5445ccc6d050e28f9a28bee206
Robin Houston <robin.houston@gmail.com>
2012-04-26 11:46:08 +0100

    Make requests.due_date a mandatory field

---
+++ db/migrate/20120426103842_change_requests_due_date_to_not_null.rb
@@ -0,0 +1,14 @@
+class ChangeRequestsDueDateToNotNull < ActiveRecord::Migration
+  def up
+    Request.where("due_date is null").find_each do |request|
+      date_received = request.date_received || request.created_at.to_date
+      request.due_date = date_received + 21.days
+      request.save!
+    end
+    change_column_null(:requests, :due_date, false)
+  end
+
+  def down
+    change_column_null(:requests, :due_date, true)
+  end
+end
