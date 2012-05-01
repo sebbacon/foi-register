21fe1224e7ab0c8806e35b9d1efdae249cc0995e
Robin Houston <robin.houston@gmail.com>
2012-05-01 17:31:35 +0100

    Change redirect after login
    After login, redirect to /admin/requests.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     staff_member = StaffMember.find_by_email(params[:email])
     if staff_member && staff_member.authenticate(params[:password])
       session[:staff_member_id] = staff_member.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to "/admin/requests", :notice => "Logged in"
     else
       flash.now.alert = "Invalid email or password"
       render "new"
