18a95a8694122f3c5e110c8cfd634989715b5853
Robin Houston <robin.houston@gmail.com>
2012-05-01 16:44:03 +0100

    The log out link should be a log in link if no one is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :require_login
 
-  private
-
   def current_staff_member
     @current_staff_member ||= StaffMember.find(session[:staff_member_id]) if session[:staff_member_id]
   end
 
+  private
   def require_login
     logger.info "#{self.class} has @does_not_require_login = #{@does_not_require_login}"
     if !@does_not_require_login && current_staff_member.nil?
